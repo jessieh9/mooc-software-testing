54ca14a3de663b58c86e8dee6c6910f39eee855f
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2023-04-13 17:49:59 -0700

    in class activity

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -8,20 +8,32 @@ public class GettingStartedTest {
     @Test
     public void addFiveTo20() {
         int result = new GettingStarted().addFive(20);
-        Assertions.assertEquals(25,result);
+        Assertions.assertEquals(25, result);
     }
 
-//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+    // UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
+    }
+
+    // Don't do this -- if the first assert fails the whole test will fail but we
+    // want to see specific input not the whole thing
+    @Test
+    public void badTests() {
+        GettingStarted g = new GettingStarted();
+        Assertions.assertEquals(5, g.addFive(0));
+        Assertions.assertEquals(6, g.addFive(1));
+        Assertions.assertEquals(7, g.addFive(2));
+        Assertions.assertEquals(8, g.addFive(3));
+        Assertions.assertEquals(9, g.addFive(4));
+        Assertions.assertEquals(10, g.addFive(5));
+    }
 }
